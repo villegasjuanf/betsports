0d0436eb59e0a8a5a84f87ccc7c025e82df6e7cc
juan villegas <juan.villegas@proactivity-ai.com>
2026-01-03 09:51:01 -0500

    remove svelte front

--- frontend/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import MainBar from "$lib/components/MainBar.svelte";
-
-
-</script>
-
-<MainBar />
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
